c92d4414b522313c55bea2264ec756da0dceb44b
John Crossley <jonnysnip3r@gmail.com>
2025-10-04 13:23:09 +0100

    Remove bottle info

--- Formula/tusk.rb
+++ Formula/tusk.rb
@@ -5,12 +5,6 @@ class Tusk < Formula
   sha256 "c2fbb8c2e9b915afbad1605c889b05cd9b36fd6a04f6d91dbbc6f47923969bd3"
   license "MIT"
 
-  bottle do
-    root_url "https://github.com/john-crossley/homebrew-tap/releases/download/tusk-0.1.1"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "544548386cb2df5eff54674260a7f0df53287f7a92fbafb036942546aebc0072"
-  end
-
   depends_on "rust" => :build
 
   def install
